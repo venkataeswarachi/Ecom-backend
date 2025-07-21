97e2516055dacd1937b1825d6bb4a37416a4fdb1
Venkata Eswar Achi <145089724+venkataeswarachi@users.noreply.github.com>
2025-07-21 15:30:20 +0530

    Update PaymentRepository.java

--- src/main/java/com/example/Ecommerce/repository/PaymentRepository.java
+++ src/main/java/com/example/Ecommerce/repository/PaymentRepository.java
@@ -2,6 +2,6 @@ package com.example.Ecommerce.repository;
 
 import com.example.Ecommerce.models.PaymentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface PaymentRepository extends JpaRepository<PaymentModel,Long> {
 }
