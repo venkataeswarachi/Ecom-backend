d4a330cc1bf2ffcaa4fdc17cd789c1b8b5f230f4
Venkata Eswar Achi <145089724+venkataeswarachi@users.noreply.github.com>
2025-07-21 15:29:31 +0530

    Update UserRepository.java

--- src/main/java/com/example/Ecommerce/repository/UserRepository.java
+++ src/main/java/com/example/Ecommerce/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.example.Ecommerce.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<UserModel,Long> {
 
     Optional<UserModel> findByEmail(String username);
